05c952a62ad85e1b36b22fc162a48aed2b55c9e3
chebotarevdmitr <127180093+chebotarevdmitr@users.noreply.github.com>
2023-08-09 23:06:03 +0300

    if you need more elements

--- dinamic_arrays_objects.cpp
+++ dinamic_arrays_objects.cpp
@@ -10,6 +10,7 @@ public:
 	float Programming_greid;
 
 };
+
 void print_students(Student* students, int size) {
 	for (int i = 0; i < size; i++) {
 		cout << "STUDENT" << i + 1 << "\t";
@@ -22,28 +23,75 @@ void print_students(Student* students, int size) {
 
 int main()
 {
+	printf("\tDINAMIC ARRAYS OF OBJECTS\n");
 	int size;
-	cout << "Number of students";
+	cout << "Number of student";
 	cin >> size;
 	Student* students = new Student[size];
 	for (int i = 0; i < size; i++) {
-		cout << "STUDENT" << i+1 << endl;
+		cout << "STUDENT " << i+1  << endl;
+
 		Student s;
-		cout << "Name:"; 
+		cout << "Name: ";
 		cin >> s.Name;
-		cout << "Age:";
+		cout << "Age: ";
 		cin >> s.Age;
-		cout << "Gender:";
+		cout << "Gtnder: ";
 		cin >> s.Gender;
-		cout << "Programming greid:";
+		cout << "Programming greid: ";
 		cin >> s.Programming_greid;
- 
+
 		students[i] = s;
 	}
 
-	print_students(students, size);
+	char choice;
+	cout << "Do you want a bigger array?";//-> может тебе нужен массив побольше?
+	cin >> choice;
+	if (choice == 'n' || choice == 'N') {
+		delete[] students;
+		return 0;
+	}
 
-	delete[] students;
+	int newSize;
+	cout << "Enter newSize: ";
+	cin >> newSize;
+	/*
+	  1.allocate newStudents array   //-> создать новый newStudents массив
+	  2.copy elements from studens   //-> копировать элементы из students 
+	  array into newStudents array   //-> массива в newStudents массмв
+	  3.delete [] students           //-> удалить [] students
+	  4.students should point to the //-> студенты должны указать на
+	  same address as newStudents    //-> тот же адрес, что и у newStudents
+	  (students = newStudents)
+	*/
+
+	// throw exceptions //-> прописать исключения
+	Student* newStudents = new Student[newSize]; // 1...
+	// 1. approach-option / первый подход - вариант функция memcpy()/ -> memcpy(newStudents, students, sizeof(Student) * size);// 2...
+	for (int i = 0; i < size; i++) {// 2. manual copying / подход ручное копирование 
+		newStudents[i] = students[i];//copy elements
+	}
+	delete [] students;
+	students = newStudents;
+
+	for (int i = size; i < newSize; i++) {
+		cout << "STUDENT " << i + 1 << endl;
+
+		Student s;
+		cout << "Name: ";
+		cin >> s.Name;
+		cout << "Age: ";
+		cin >> s.Age;
+		cout << "Gtnder: ";
+		cin >> s.Gender;
+		cout << "Programming greid: ";
+		cin >> s.Programming_greid;
+
+		students[i] = s;
+	}
+	print_students(students, newSize);
+
+	delete [] students;
    cin.get();
 }
 
